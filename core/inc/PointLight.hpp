44c752985f197a6bb1eaba2f3fb46e7ae2c33a7a
Roberto Cano <gabr1e11@hotmail.com>
2016-05-18 18:26:16 +0200

    Rolled back locally all changes making lights not to work

--- core/inc/PointLight.hpp
+++ core/inc/PointLight.hpp
@@ -12,15 +12,11 @@
 class PointLight : public Light
 {
 	public:
-		PointLight(float attenuation = 1.0f) { _attenuation = attenuation; }
-		PointLight(const glm::vec3 &ambient  = glm::vec3(0.0f, 0.0f, 0.0f),
-			       const glm::vec3 &diffuse  = glm::vec3(0.0f, 0.0f, 0.0f),
-			       const glm::vec3 &specular = glm::vec3(0.0f, 0.0f, 0.0f),
-			       const glm::vec3 &position = glm::vec3(0.0f, 0.0f, 0.0f),
-			       float attenuation = -1.0f,
-			       float cutoff = -1.0f,
-			       float coneAngle = 360.0f) :
-			Light(ambient, diffuse, specular, position, attenuation, cutoff, coneAngle) {
+		PointLight(const glm::vec3 &ambient,
+			       const glm::vec3 &diffuse,
+			       const glm::vec3 &specular,
+			       const glm::vec3 &position) :
+			Light(ambient, diffuse, specular, position) {
         }
 		~PointLight() { }
 };
