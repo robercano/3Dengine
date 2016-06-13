c11d87bd5a7c07b95f2d167148eae0d0d39135c6
Roberto Cano <gabr1e11@hotmail.com>
2016-06-13 18:31:22 +0200

    Documentatin of the sources

--- core/inc/Model3D.hpp
+++ core/inc/Model3D.hpp
@@ -1,6 +1,40 @@
 /**
  * @class	Model3D
- * @brief	Basic model 3D representation
+ * @brief	A Model3D represents a 3D asset in the engine including the geometry,
+ *          normals, textures and texture coordinates, and vertices indices for
+ *          indexed geometry rendering. This class can be expanded to support any
+ *          3D model definition that can be needed in the engine, like displacement
+ *          maps. This class represents an static model and does not take into
+ *          account animations or tessellation of skin models.
+ *
+ *          Objects of this class cannot be instantiated directly and cannot only
+ *          be obtained through model loaders.
+ *
+ *          The data of the 3D model is organized as follows:
+ *
+ *              * List of vertices of the model including position, normal and UV coordinate.
+ *                If a vertex belongs to 2 different polygons with different textures or surface
+ *                directions, the same vertex position can appear more than once with different
+ *                normal and/or different UV coordinate
+ *              * List of indices for triangle rendering lists. The list of indices is always
+ *                a multiple of 3 and it specifies how to group 3 vertices to form a triangle,
+ *                including the face direction (counter clock-wise)
+ *              * The following vector define the rendering lists. Each entry of the folowing vectors
+ *                define 1 display list of triangles with it's corresponding material and texture. All
+ *                the following vectors must have the same length:
+ *
+ *                  * List of textures
+ *                  * List of materials
+ *                  * List of offsets into the indices list for the start of the rendering list
+ *                  * List with the number of indices that belong to the rendering list starting
+ *                    at the indicated offset
+ *
+ *          The former data allows to render a model3D as a group of rendering lists, with each list
+ *          defining a material and a texture, and a list of indices into the raw vertex data. This
+ *          minimizes the bandwidth required from the GPU to render the model.
+ *
+ *          TODO: This class is likely to be refactored when animations are introduced
+ *          in the engine
  *
  * @author	Roberto Cano (http://www.robertocano.es)
  */
@@ -16,18 +50,38 @@
 class Model3D : public Object3D
 {
   public:
+    /**
+     * Vertex data of the model
+     */
     typedef struct {
-        glm::vec3 vertex;
-        glm::vec3 normal;
-        glm::vec2 uvcoord;
+        glm::vec3 vertex;  /**< Vertex in model coordinates */
+        glm::vec3 normal;  /**< Normal of the vertex */
+        glm::vec2 uvcoord; /**< Textures coordinates of the vertex */
     } VertexData;
 
     /**
      * Destructor
      */
     virtual ~Model3D() {}
+    /**
+     * Normalizes all vertex data in the model
+     *
+     * This method calculates the center of mass of all vertices in the model
+     * and then normalizes all vertices with respect to this CoM so the maximum length
+     * from a vertex position to the CoM is 1.0f. This is typically used after loading
+     * a model from disk which hasn't been imported before in the engine so the user can
+     * make sure the model will be visible on screen
+     */
     void normalize();
 
+    /**
+     * Getter
+     *
+     * All getter return a reference to one of the internal vectors containing
+     * the model definition for a specific subset of the data. The data is layed
+     * out consecutively on memory so the caller can obtain the pointer to the
+     * first element and load the buffer into the GPU
+     */
     const std::vector<Model3D::VertexData> &getVertexData() const { return _modelData; }
     const std::vector<uint32_t> &getIndexData() const { return _modelIndices; }
     const std::vector<Material> &getMaterials() const { return _materials; }
@@ -35,16 +89,25 @@ class Model3D : public Object3D
     const std::vector<uint32_t> &getIndicesOffsets() const { return _indicesOffsets; }
     const std::vector<uint32_t> &getIndicesCount() const { return _indicesCount; }
   protected:
+    /**
+     * Constructor
+     */
     Model3D() {}
+    /**
+     * Convenience function to calculate the center of mass of the model
+     *
+     * It loops through all vertices in the model assuming the same weight for
+     * all of them and then finds the center of mass
+     *
+     * @param cm   Out parameter containing the center of mass for the current
+     *             set of vertices
+     */
     void _calculateCenterOfMass(glm::vec3 &cm);
 
-    /**
-     * Object data
-     */
-    std::vector<Model3D::VertexData> _modelData;
-    std::vector<uint32_t> _modelIndices;
-    std::vector<Material> _materials;
-    std::vector<Texture> _textures;
-    std::vector<uint32_t> _indicesOffsets;
-    std::vector<uint32_t> _indicesCount;
+    std::vector<Model3D::VertexData> _modelData; /**< Data containing the vertex position, normal and UV coordinates */
+    std::vector<Material> _materials;            /**< List of materials used in the model */
+    std::vector<Texture> _textures;              /**< List of textures used in the model */
+    std::vector<uint32_t> _modelIndices;         /**< List of indices containing all rendering lists together */
+    std::vector<uint32_t> _indicesOffsets;       /**< Offset in _modelIndices of the beginning of the rendering list number 'n' */
+    std::vector<uint32_t> _indicesCount;         /**< Number of indices belonging to the rendering list number 'n' */
 };
--- core/inc/Object3D.hpp
+++ core/inc/Object3D.hpp
@@ -1,7 +1,8 @@
 /**
  * @class	Object3D
- * @brief	Meta-class that represents any object in the 3D
- *          world that has a position and orientation
+ * @brief	Meta-class that represents any object in the 3D world that has a
+ *          position, an orientation and a scale factor. Both the model
+ *          and the view matrix can be obtained from any Object3D instance
  *
  * @author	Roberto Cano (http://www.robertocano.es)
  */
@@ -15,6 +16,9 @@
 class Object3D
 {
   public:
+    /**
+     * Constructor
+     */
     Object3D()
         : _position(0.0f, 0.0f, 0.0f)
         , _orientation(glm::mat4(1.0f))
@@ -25,6 +29,75 @@ class Object3D
     {
     }
 
+    /**
+     * Moves the postition of the 3D object according to the given amount for each axis
+     *
+     * Forces recalculation of the model and view matrices
+     *
+     * @param amount  Amount of movement on each axis as a vec3
+     */
+    void move(const glm::vec3 &amount)
+    {
+        _position += amount;
+        _modelValid = false;
+        _viewValid = false;
+    }
+
+    /**
+     * Rotates the current orientation according to the given rotation as a quaternion
+     *
+     * Forces recalculation of the model and view matrices
+     *
+     * @param rotation  Rotation to apply to the current orientation as a mat4 quaternion
+     */
+    void rotate(const glm::mat4 &rotation)
+    {
+        _orientation = rotation * _orientation;
+        _modelValid = false;
+        _viewValid = false;
+    }
+
+    /**
+     * Multiplies the current scale factor by the given scale factor and sets
+     * the result as the new scale factor
+     *
+     * Forces recalculation of the model and view matrices
+     *
+     * @param factor  Scale factor to apply to the 3D object current scale factor
+     */
+    void scale(const glm::vec3 &factor)
+    {
+        _scale *= factor;
+        _modelValid = false;
+        _viewValid = false;
+    }
+
+    /**
+     * Recalculates the view and model matrices so the object's forward vector
+     * goes from the object's position to the given 'at' position
+     *
+     * Forces recalculation of the model and view matrices
+     *
+     * @param at  Position in space where the forward vector will point to
+     */
+    void lookAt(const glm::vec3 &at)
+    {
+        glm::vec3 up(0.0f, 1.0f, 0.0f);
+
+        // TODO: possible problem with lights not rendering shadow map correctly
+        // when view vector is aligned with up vector
+        _view = glm::lookAt(_position, at, up);
+        _model = glm::inverse(_view);
+
+        _modelValid = true;
+        _viewValid = true;
+    }
+
+    /**
+     * Setters
+     *
+     * All setters force recalculation of the model and view matrices
+     */
     void setPosition(const glm::vec3 &position)
     {
         _position = position;
@@ -43,38 +116,27 @@ class Object3D
         _modelValid = false;
         _viewValid = false;
     }
-    void move(const glm::vec3 &amount)
-    {
-        _position += amount;
-        _modelValid = false;
-        _viewValid = false;
-    }
-    void rotate(const glm::mat4 &rotation)
-    {
-        _orientation = rotation * _orientation;
-        _modelValid = false;
-        _viewValid = false;
-    }
-    void scale(const glm::vec3 &factor)
-    {
-        _scale *= factor;
-        _modelValid = false;
-        _viewValid = false;
-    }
-    void lookAt(const glm::vec3 &at)
-    {
-        glm::vec3 up(0.0f, 1.0f, 0.0f);
 
-        // TODO: possible problem with lights not rendering shadow map correctly
-        // when view vector is aligned with up vector
-        _view = glm::lookAt(_position, at, up);
-        _modelValid = false;
-        _viewValid = true;
-    }
+    /**
+     * Getters
+     */
     const glm::vec3 &getPosition() { return _position; }
     const glm::mat4 &getOrientation() { return _orientation; }
     const glm::vec3 &getScaleFactor() { return _scale; }
+    /**
+     * Returns the current direction of the model, which is the forward vector
+     *
+     * @return A vec3 with the forward vector
+     */
     glm::vec3 getDirection() { return -glm::vec3(glm::row(getViewMatrix(), 2)); }
+    /**
+     * Returns the current model matrix of the object
+     *
+     * The matrix is cached and only recalculated if any of the setters or model
+     * matrix modifiers are called
+     *
+     * @return A mat4 with the current model matrix
+     */
     const glm::mat4 &getModelMatrix(void)
     {
         if (_modelValid == false) {
@@ -83,6 +145,15 @@ class Object3D
         }
         return _model;
     }
+
+    /**
+     * Returns the current view matrix of the object
+     *
+     * The matrix is cached and only recalculated if any of the setters or model
+     * matrix modifiers are called
+     *
+     * @return A mat4 with the current model matrix
+     */
     const glm::mat4 &getViewMatrix(void)
     {
         if (_viewValid == false) {
@@ -93,11 +164,11 @@ class Object3D
     }
 
   protected:
-    glm::vec3 _position;
-    glm::mat4 _orientation;
-    glm::vec3 _scale;
-    glm::mat4 _model;
-    glm::mat4 _view;
-    bool _modelValid;
-    bool _viewValid;
+    glm::vec3 _position;    /**< Position of the object in world coordinates */
+    glm::mat4 _orientation; /**< Orientation of the object as a quaternion */
+    glm::vec3 _scale;       /**< Scale factors for each axis XYZ */
+    glm::mat4 _model;       /**< Current model matrix of the object */
+    glm::mat4 _view;        /**< Current view matrix of the object */
+    bool _modelValid;       /**< If true, current model matrix is cached and does not need recalculation */
+    bool _viewValid;        /**< If true, current view matrix is cached and does not need recalculation */
 };
